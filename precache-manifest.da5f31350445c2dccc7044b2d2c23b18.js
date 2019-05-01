3d07291363ab56c49b4a543613dc1b1408f1c0c9
Ulises Rangel <uli.atx@gmail.com>
2019-04-30 22:54:07 -0500

    Updates

---
+++ precache-manifest.da5f31350445c2dccc7044b2d2c23b18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c99f0009139811c128c77e952f542d",
+    "url": "/urangel/z-o-lite/index.html"
+  },
+  {
+    "revision": "ab2d6b2534474be6b43b",
+    "url": "/urangel/z-o-lite/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/urangel/z-o-lite/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "ab2d6b2534474be6b43b",
+    "url": "/urangel/z-o-lite/static/js/main.46248a48.chunk.js"
+  },
+  {
+    "revision": "4de7a47319218b1e5149",
+    "url": "/urangel/z-o-lite/static/js/runtime~main.f11fd48f.js"
+  }
+]);
