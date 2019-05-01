2d50132b8eb34e1e418ba1298d558c45ea9f4f16
Ulises Rangel <uli.atx@gmail.com>
2019-05-01 12:04:14 -0500

    Updates

---
+++ precache-manifest.236c3b39c099199362364d86fafaec56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3f844b8d08931619bfe69d121a0750",
+    "url": "/z-o-lite/index.html"
+  },
+  {
+    "revision": "4e42b9adb633a8b5683f",
+    "url": "/z-o-lite/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/z-o-lite/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "4e42b9adb633a8b5683f",
+    "url": "/z-o-lite/static/js/main.4a677cbc.chunk.js"
+  },
+  {
+    "revision": "77ff6af2f19634e08b6e",
+    "url": "/z-o-lite/static/js/runtime~main.43efd397.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/urangel/z-o-lite/precache-manifest.9532832f5b1a8cbdbebf629ff8bd7331.js"
+  "/z-o-lite/precache-manifest.236c3b39c099199362364d86fafaec56.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/urangel/z-o-lite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/z-o-lite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
